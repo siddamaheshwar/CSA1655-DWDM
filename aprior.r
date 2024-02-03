3ff4e55541a435f38c3a55a91b971f25400bb858
siddamaheshwar <113339062+siddamaheshwar@users.noreply.github.com>
2024-02-03 15:00:54 +0530

    Create aprior.r

---
+++ aprior.r
@@ -0,0 +1,8 @@
+rules <- apriori(Groceries,
+parameter = list(supp = 0.01, conf = 0.2))
+inspect(rules[1:10])
+arules::itemFrequencyPlot(Groceries, topN = 20,
+col = brewer.pal(8, 'Pastel2'),
+main = 'Relative Item Frequency Plot',
+type = "relative",
+ylab = "Item Frequency (Relative)")
